6aad39773895e52a40e669eeefd20ee8626f2a59
Chris Galera <galera.chris@yahoo.com>
2021-04-27 21:24:51 -0500

    Added Styling Added Sensai Challenge

--- src/Components/MoreForms.js
+++ src/Components/MoreForms.js
@@ -14,45 +14,63 @@ const UserForm = (props) => {
         console.log("Welcome", newUser);
     };
 
+    const inputDataDivSytle = {
+        borderRadius: "5px", 
+        backgroundColor: "#e6e6e6",
+        border: "1px solid darkgrey",
+        padding: "0px 10px",
+        margin: "5px",
+    }
+    
+
     return (
         <form onSubmit= {createUser}>
-            <div>
+            <div style= {inputDataDivSytle} >
                 <label>First Name: </label>
                 <input type="text" onChange = { (e) => setFirstName(e.target.value)} value={firstName}/>
-                { firstName.length < 2 ? 
-                    <p>First Name must be at least 2 characters long</p>
-                    : ''
+                { 
+                    firstName.length > 0 ? 
+                    firstName.length < 2 ?
+                    <p className="error"> First Name must be at least 2 characters long</p>
+                    : null
+                    : null
                 }
             </div>
-            <div>
+            <div style= {inputDataDivSytle} >
                 <label>Last Name: </label>
                 <input type="text" onChange = { (e) => setLastName(e.target.value)} value = {lastName}/>
-                { lastName.length < 2 ? 
-                    <p>First Name must be at least 2 characters long</p>
-                    : ''
+                { lastName.length > 0 ?
+                    lastName.length < 2 ? 
+                    <p className="error"> First Name must be at least 2 characters long</p>
+                    : null
+                    : null
                 }
             </div>
-            <div>
+            <div style= {inputDataDivSytle} >
                 <label>Email: </label>
                 <input type="text" onChange = { (e) => setEmail(e.target.value)} value = {email}/>
-                { email.length < 2 ? 
-                    <p>Email must be at least 2 characters long</p>
-                    : ''
+                { email.length > 0?
+                    email.length < 5 ? 
+                    <p className="error"> Email must be at least 2 characters long</p>
+                    : null
+                    : null
                 }
             </div>
-            <div>
+            <div style= {inputDataDivSytle} >
                 <label>Password: </label>
                 <input type="text" onChange = { (e) => setPassword(e.target.value)} value = {password}/>
-                { password.length < 2 ? 
-                    <p>Passwords must be at least 8 characters long</p>
-                    : ''
+                { password.length > 0 ?
+                    password.length < 2 ? 
+                    <p className="error"> Passwords must be at least 8 characters long</p>
+                    : null
+                    : null
                 }
             </div>
-            <div>
+            <div style= {inputDataDivSytle} >
                 <label>Confirm Password: </label>
                 <input type="text" onChange = { (e) => setConfirmPassword(e.target.value)} value = {confirmPassword}/>
                 { confirmPassword !== password ? 
-                    <p>Passwords must match</p>
+                    <p className="error"> Passwords must match</p>
                     : ''
                 }
             </div>
